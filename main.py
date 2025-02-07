31cd50caa5e26eadcf70f7c64bcdff9fcd0ba1e5
Fayerowsky <michal-bujok@o2.pl>
2025-02-07 22:12:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -51,18 +51,18 @@ class Application:
     def create_main_ui(self):
         self.root.geometry("700x400")
 
-        self.ui_frame = tk.Frame(self.root)
+        self.ui_frame = tk.Frame(self.root, bg="gray")
         self.ui_frame.pack(side="top", fill="both", expand=True)
 
-        self.side_frame = tk.Frame(self.ui_frame, width=100, bg="red")
+        self.side_frame = tk.Frame(self.ui_frame, width=100, bg="gray")
         self.side_frame.pack(side="left", fill="y")
         self.side_frame.pack_propagate(False)
 
-        self.main_frame = tk.Frame(self.ui_frame, width=600, bg="pink")
+        self.main_frame = tk.Frame(self.ui_frame, width=600)
         self.main_frame.pack(side="right", fill="both", expand=True)
         self.main_frame.pack_propagate(False)
 
-        self.add_btn = tk.Button(self.side_frame, text="Add", command=self.create_add_ui, state="active")
+        self.add_btn = ttk.Button(self.side_frame, text="Add", command=self.create_add_ui, state="active")
         self.add_btn.pack(side="bottom", pady=10)
 
         try:
@@ -99,7 +99,7 @@ class Application:
         self.add_btn.config(state="active")
 
     def create_add_ui(self):
-        self.add_frame = tk.Frame(self.root, bg="green")
+        self.add_frame = tk.Frame(self.root)
         self.add_frame.pack(side="bottom", fill="both", pady=5, padx=5)
 
         add_var1 = tk.StringVar()
@@ -110,11 +110,11 @@ class Application:
         self.username_label = tk.Label(self.add_frame, text="username").grid(row=1, column=0)
         self.password_label = tk.Label(self.add_frame, text="password").grid(row=2, column=0)
 
-        self.add_name = tk.Entry(self.add_frame, textvariable=add_var1, width=65).grid(row=0, column=1)
-        self.add_username = tk.Entry(self.add_frame, textvariable=add_var2, width=65).grid(row=1, column=1)
-        self.add_password = tk.Entry(self.add_frame, textvariable=add_var3, width=65).grid(row=2, column=1)
+        self.add_name = ttk.Entry(self.add_frame, textvariable=add_var1, width=65).grid(row=0, column=1)
+        self.add_username = ttk.Entry(self.add_frame, textvariable=add_var2, width=65).grid(row=1, column=1)
+        self.add_password = ttk.Entry(self.add_frame, textvariable=add_var3, width=65).grid(row=2, column=1)
 
-        submit_btn = tk.Button(self.add_frame, text="Submit", command=lambda: self.submit_func(add_var1.get(), add_var2.get(), add_var3.get()))
+        submit_btn = ttk.Button(self.add_frame, text="Submit", command=lambda: self.submit_func(add_var1.get(), add_var2.get(), add_var3.get()))
         submit_btn.grid(row=3, column=1, pady=10)
 
         self.add_btn.config(state="disabled")
@@ -123,5 +123,3 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = Application(root)
     root.mainloop()
-
-    ##
